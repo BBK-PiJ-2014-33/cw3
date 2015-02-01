52c0a0fb1e64b274de309d8906d7d829cc1526aa
BBK-PiJ-2014-33 <ludak303@gmail.com>
2015-02-01 20:08:19 +0000

    StackImpl implemented

---
+++ src/Tests/StackImplTests.java
@@ -0,0 +1,15 @@
+package Tests;
+
+import Stack.*;
+import org.junit.*;
+import List.*;
+
+public class StackImplTests {
+
+    @Test
+    public void sampleTest()
+    {
+        List myList = new LinkedList();
+        Stack myStack = new StackImpl(myList);
+    }
+}
